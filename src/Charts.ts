2fd98fb6295cc7c92bbe99ff5a1722db925b3456
Björn Urban <burban@inovex.de>
2022-07-13 17:39:39 +0200

    Fix keyref password key

--- src/Charts.ts
+++ src/Charts.ts
@@ -78,7 +78,7 @@ export function createDirectus() {
             {name: "DB_CLIENT", value: "mysql"},
             {name: "DB_HOST", value: "https://directus-release-mariadb"},
             {name: "DB_PORT", value: "3306"},
-            {name: "DB_PASSWORD", valueFrom: {secretKeyRef: {name:"directus-release-mariadb", key: "password"}}},
+            {name: "DB_PASSWORD", valueFrom: {secretKeyRef: {name:"directus-release-mariadb", key: "mariadb-password"}}},
             {name: "DB_USER", value: "directus"},
             {name: "DB_DATABASE", value: "directus"}
             // etc
