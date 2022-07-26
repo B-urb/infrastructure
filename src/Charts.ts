323af2ee5d441c77b482ae4b2b494fba469dad0c
Björn Urban <burban@inovex.de>
2022-07-26 13:20:17 +0200

    Update minio port

--- src/Charts.ts
+++ src/Charts.ts
@@ -89,7 +89,7 @@ export function createDirectus() {
             {name: "STORAGE_S3_KEY", valueFrom: {secretKeyRef: {name:directusS3Secret.metadata.name, key:"user-key"}}},
             {name: "STORAGE_S3_SECRET", valueFrom: {secretKeyRef: {name: directusS3Secret.metadata.name, key:"user-secret"}}},
             {name: "STORAGE_S3_BUCKET", value: "directus"},
-            {name: "STORAGE_S3_ENDPOINT", value: "https://minio.fbr.ai/buckets/directus/"},
+            {name: "STORAGE_S3_ENDPOINT", value: "https://minio.fbr.ai:9000"},
             {name: "STORAGE_S3_S3_FORCE_PATH_STYLE", value: 'true'}
             // etc
           ],
