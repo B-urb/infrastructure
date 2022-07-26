577328b1a8e4f4cd241a147aa4488beb3ca0208e
Björn Urban <burban@inovex.de>
2022-07-26 13:12:48 +0200

    Update endpoint variable

--- src/Charts.ts
+++ src/Charts.ts
@@ -88,8 +88,9 @@ export function createDirectus() {
             {name: "STORAGE_S3_DRIVER", value: "s3" },
             {name: "STORAGE_S3_KEY", valueFrom: {secretKeyRef: {name:directusS3Secret.metadata.name, key:"user-key"}}},
             {name: "STORAGE_S3_SECRET", valueFrom: {secretKeyRef: {name: directusS3Secret.metadata.name, key:"user-secret"}}},
-            {name: "STORAGE_S3_BUCKET", value: "https://minio.fbr.ai/buckets/directus/"},
-            {name: "STORAGE_S3_ENDPOINT", value: "https://minio.fbr.ai/"}
+            {name: "STORAGE_S3_BUCKET", value: "directus"},
+            {name: "STORAGE_S3_ENDPOINT", value: "https://minio.fbr.ai/buckets/directus/"},
+            {name: "STORAGE_S3_S3_FORCE_PATH_STYLE", value: 'true'}
             // etc
           ],
           "mariadb": {
