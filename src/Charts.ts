72355a6fb49ffe8c0391fcc8835c1cbb033e0707
Björn Urban <burban@inovex.de>
2022-07-27 08:34:41 +0200

    Update env

--- src/Charts.ts
+++ src/Charts.ts
@@ -88,9 +88,9 @@ export function createDirectus() {
             {name: "STORAGE_S3_DRIVER", value: "s3" },
             {name: "STORAGE_S3_KEY", valueFrom: {secretKeyRef: {name:directusS3Secret.metadata.name, key:"user-key"}}},
             {name: "STORAGE_S3_SECRET", valueFrom: {secretKeyRef: {name: directusS3Secret.metadata.name, key:"user-secret"}}},
-            {name: "STORAGE_S3_BUCKET", value: "https://minio.fbr.ai/directus"},
+            {name: "STORAGE_S3_BUCKET", value: "directus"},
             {name: "STORAGE_S3_REGION", value: ""},
-            {name: "STORAGE_S3_ENDPOINT", value:  "minio.fbr.ai:80"}, //"https://minio.fbr.ai:80"},
+            {name: "STORAGE_S3_ENDPOINT", value: "https://minio.fbr.ai"},
             {name: "STORAGE_S3_S3_FORCE_PATH_STYLE", value: 'true'},
             {name: "STORAGE_S3_SIGNATURE_VERSION", value: "v4"},
             {name: "LOG_LEVEL", value: "trace"}
