0d98eb1daaa80cd5b0787602591fd1421b61214f
Björn Urban <burban@inovex.de>
2022-09-14 16:45:54 +0200

    feat: new experimental image

--- index.ts
+++ index.ts
@@ -8,11 +8,13 @@ import createCronjob from "./src/CronJob";
 
 
 let cronjob = createCronjob()
+let keelAnnotationsExp = {"keel.sh/policy": "all"}
 let keelAnnotationsDev = {"keel.sh/match-tag":"true", "keel.sh/policy": "all"}
 let keelAnnotationsProd = {"keel.sh/policy": "major"}
 
 let ws1 = new WebService("webcv-dev","dev.burban.me", "registry.gitlab.com/privateprojectsbu/webcv", "v1.2.2-rc.2", keelAnnotationsDev);
 let ws2 = new WebService("webcv-prod", "burban.me",  "registry.gitlab.com/privateprojectsbu/webcv", "v1.1.2", keelAnnotationsProd);
+let ws3 = new WebService("webcv-experimental", "experimental.burban.me","registry.gitlab.com/privateprojectsbu/webcv","feature-next", keelAnnotationsExp);
 
 createDeployments(new Array<WebService>(ws1, ws2));
 createService(new Array<WebService>(ws1, ws2));
