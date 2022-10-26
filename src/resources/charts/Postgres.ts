ab8dd86030922e95cfbe9bdb381bd61996a444f4
Björn Urban <burban@inovex.de>
2022-10-26 21:48:58 +0200

    fix: postgres deployment

--- src/resources/charts/Postgres.ts
+++ src/resources/charts/Postgres.ts
@@ -7,8 +7,8 @@ const dbRootPassword = process.env.CI_DB_ROOT_PASSWORD;
 
 
 export function createPostgres(namespace: Namespace) {
-  return new k8s.helm.v3.Chart("directus-release", {
-        chart: "postgres",
+  return new k8s.helm.v3.Chart("postgres", {
+        chart: "postgresql",
         namespace: namespace.metadata.name,
         fetchOpts: {
           repo: "https://charts.bitnami.com/bitnami",
