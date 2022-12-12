54dd80547e2b9ff0af13c80d70fd632a440928b9
Björn Urban <burban@inovex.de>
2022-12-12 12:07:24 +0100

    fix: container

--- src/contributors/bjoern.ts
+++ src/contributors/bjoern.ts
@@ -19,7 +19,7 @@ export function createBjoern() {
  let ws2 = new WebService("webcv-prod", "burban.me", namespaceBurban, "registry.gitlab.com/privateprojectsbu/webcv", "v2.4.0", {}, "prod");
  let ws3 = new WebService("webcv-experimental", "experimental.burban.me", namespaceBurban, "registry.gitlab.com/privateprojectsbu/webcv", "feature-next", basicAuthAnnotation, "experimental");
  let ws4 = new WebService("website-dev", "dev.tischlerei-bahrenberg.de", namespaceBahrenberg, "registry.gitlab.com/a9668/bahrenberg/website", "v1.0.0-rc.9", basicAuthAnnotation, "dev");
- let ws5 = new WebService("flathunter", "flat.burban.me", namespaceBurban, "bjoern5urban/flathunter", "latest", basicAuthAnnotation, "experimental");
+ let ws5 = new WebService("flathunter", "flat.burban.me", namespaceBurban, "registry.gitlab.com/privateprojectsbu/flathunter", "latest", basicAuthAnnotation, "experimental");
  const gitlabSecretB = createGitlabSecret("pulumi", env.pullSecret, "gitlab-pull-secret", namespaceBurban);
  const gitlabSecretBa = createGitlabSecret("pulumi", env.pullSecret, "gitlab-pull-secret-bahrenberg", namespaceBahrenberg);
 
