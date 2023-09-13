efb06f35eb3a421d1457d6cab4bdf6f96dfd95fe
Felix Breuer <fbreuer@pm.me>
2023-09-13 12:04:51 +0200

    add lychee secret
    Signed-off-by: Felix Breuer <fbreuer@pm.me>

--- l2/index.ts
+++ l2/index.ts
@@ -1,14 +1,14 @@
-import {createDirectus} from "./directus";
-import {createNamespace} from "./namespace";
-import {createUmami} from "./umami";
-import {createDirectusConfigMap } from "./configs";
-import {DirectusConfig} from "../util/types";
+import { createDirectus } from "./directus";
+import { createNamespace } from "./namespace";
+import { createUmami } from "./umami";
+import { createDirectusConfigMap } from "./configs";
+import { DirectusConfig } from "../util/types";
 import * as postgresql from "@pulumi/postgresql";
-import {Provider, Role} from "@pulumi/postgresql";
-import {RandomPassword} from "@pulumi/random";
-import {Config, getStack, interpolate, jsonParse, log, Output, StackReference} from "@pulumi/pulumi";
-import {createBackupSecret, createDirectusSecret, createUmamiSecret} from "./secrets";
-import {Namespace} from "@pulumi/kubernetes/core/v1";
+import { Provider, Role } from "@pulumi/postgresql";
+import { RandomPassword } from "@pulumi/random";
+import { Config, getStack, interpolate, jsonParse, log, Output, StackReference } from "@pulumi/pulumi";
+import { createBackupSecret, createDirectusSecret, createUmamiSecret } from "./secrets";
+import { Secret } from "@pulumi/kubernetes/core/v1";
 import createBackupCronjob from "./CronJob";
 
 const config = new Config();
@@ -23,60 +23,79 @@ const password = new RandomPassword("directusDbPassword", {
   length: 16,
   special: true,
 });
-const postgresNamespace = stackRef.getOutput("postgresNamespace").apply(namespace =>  interpolate `${namespace}`)
+const postgresNamespace = stackRef.getOutput("postgresNamespace").apply(namespace => interpolate`${namespace}`)
 //Initialize Postgres Provider. NOTE: Requires port-forward for now
-const dbAuthPassword = stackRef.getOutput("postgresRootPassword").apply(authPW => interpolate `${authPW}`)
-const mailgunKey = stackRef.getOutput("mailgunKey").apply(authPW => interpolate `${authPW}`)
-const postgresUrl = stackRef.getOutput("postgresUrl").apply(url =>  interpolate `${url}`)
+const dbAuthPassword = stackRef.getOutput("postgresRootPassword").apply(authPW => interpolate`${authPW}`)
+const mailgunKey = stackRef.getOutput("mailgunKey").apply(authPW => interpolate`${authPW}`)
+const postgresUrl = stackRef.getOutput("postgresUrl").apply(url => interpolate`${url}`)
 
 
 
 
-const postgresProvider = new Provider("custom", {host: postgresUrl , password: dbAuthPassword, username: "postgres", sslmode:"disable"})
+const postgresProvider = new Provider("custom", { host: postgresUrl, password: dbAuthPassword, username: "postgres", sslmode: "disable" })
 
 // Create Backup for Database
 const backupPGPassword = new RandomPassword("backupPGPassword", {
   length: 16,
   special: true,
 });
-const backupRole = new Role("backup", {login: true,name:"backup", password: backupPGPassword.result, superuser: true, replication: true}, {provider:postgresProvider});
+const backupRole = new Role("backup", { login: true, name: "backup", password: backupPGPassword.result, superuser: true, replication: true }, { provider: postgresProvider });
 const backUpSecret = {
   "db-user": backupRole.name,
   "db-password": backupPGPassword.result,
-  "s3-user-key":  config.get("s3-key")!!,
+  "s3-user-key": config.get("s3-key")!!,
   "s3-user-secret": config.get("s3-secret")!!,
 }
 
 createBackupCronjob(postgresNamespace, createBackupSecret(postgresNamespace, backUpSecret))
 export const backupPassword = backupPGPassword.result
+
+function createDBCredentials(ident: string) {
+  const password = new RandomPassword(`${ident}DBPassword`, {
+    length: 26,
+    special: false,
+  });
+  const db = new postgresql.Database(ident, { name: ident }, { provider: postgresProvider });
+
+  const role = new Role(ident, { login: true, name: ident, password: password.result }, { provider: postgresProvider });
+  new postgresql.Grant(`${ident}Full`, {
+    database: db.name,
+    objectType: "database",
+    privileges: ["ALL"],
+    role: role.name,
+  }, { provider: postgresProvider });
+
+  return {
+    user: role.name,
+    password: password.result,
+    db: db.name,
+  }
+}
+
 // Create Database for DirectusCMS
-const directusDb = new postgresql.Database("directus", {name: "directus"},{provider:postgresProvider});
-const role = new Role("directus", {login: true, name: "directus", password: password.result}, {provider:postgresProvider});
-const grant  = new postgresql.Grant("directusFull", {
+const directusDb = new postgresql.Database("directus", { name: "directus" }, { provider: postgresProvider });
+const role = new Role("directus", { login: true, name: "directus", password: password.result }, { provider: postgresProvider });
+const grant = new postgresql.Grant("directusFull", {
   database: directusDb.name,
   objectType: "database",
   privileges: ["ALL"],
   role: role.name,
-}, {provider:postgresProvider});
+}, { provider: postgresProvider });
 
+const umamiCredentials = createDBCredentials("umami")
 
-//Create Database for umami
-const umamiPassword = new RandomPassword("umamiDBPassword", {
-  length: 26,
-  special: false,
-});
-const umamiDb = new postgresql.Database("umami", {name: "umami"},{provider:postgresProvider});
+const lycheeIdent = "lychee"
+const lycheeCredentials = createDBCredentials(lycheeIdent)
+const lycheeNS = createNamespace(lycheeIdent)
 
-const umamiRole = new Role("umami", {login: true,name:"umami", password: umamiPassword.result}, {provider:postgresProvider});
-const umami  = new postgresql.Grant("umamiFull", {
-  database: umamiDb.name,
-  objectType: "database",
-  privileges: ["ALL"],
-  role: umamiRole.name,
-}, {provider:postgresProvider});
-
-
-
+new Secret(lycheeIdent, {
+  metadata: {
+    name: `db-credentials`,
+    namespace: lycheeNS.metadata.name,
+  },
+  type: "Opaque",
+  stringData: { ...lycheeCredentials },
+})
 
 // Create apps for general usage
 export const namespaceMedusa = createNamespace("medusa")
@@ -84,13 +103,13 @@ export const namespaceUmami = createNamespace("umami")
 export const namespaceDirectus = createNamespace("directus")
 
 
-export const adminPassword = new RandomPassword("admin-password", {length: 19, special: true}).result.apply(
-    password => interpolate `${password}`
+export const adminPassword = new RandomPassword("admin-password", { length: 19, special: true }).result.apply(
+  password => interpolate`${password}`
 )
-const directusSecretKey = new RandomPassword("directus-secret", {length: 24}).result.apply(
-    password => interpolate `${password}`
+const directusSecretKey = new RandomPassword("directus-secret", { length: 24 }).result.apply(
+  password => interpolate`${password}`
 )
- const directusSecret = {
+const directusSecret = {
   "db-user": role.name,
   "db-password": password.result,
   "admin-mail": config.get("admin-mail")!!,
@@ -117,6 +136,6 @@ const directusConfig: DirectusConfig = {
 }
 const directus = createDirectus("manual", directusConfig);
 export const umamiSecret = {
-  "db-connection-string": interpolate `postgresql://${umamiRole.name}:${umamiPassword.result}@postgres-postgresql.postgres:5432/${umamiDb.name}`
+  "db-connection-string": interpolate`postgresql://${umamiCredentials.user}:${umamiCredentials.password}@postgres-postgresql.postgres:5432/${umamiCredentials.db}`
 }
 createUmami("manual", namespaceUmami, createUmamiSecret(namespaceUmami, umamiSecret))
