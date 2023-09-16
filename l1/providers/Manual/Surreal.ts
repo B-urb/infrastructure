32eeab3cd4a5cd67a0f25aad66211c55408f7c36
Björn Urban <burban@inovex.de>
2023-09-17 00:27:58 +0200

    fix: startup

--- l1/providers/Manual/Surreal.ts
+++ l1/providers/Manual/Surreal.ts
@@ -80,7 +80,7 @@ export function createSurrealManual() {
        "name": name,
        "image": image + ":" + tag,
        "imagePullPolicy": "Always",
-       "args": ["start", "--log", "$(LOG_LEVEL)", "--user", "$(SURREAL_USER)", "--pass", "$(SURREAL_PASSWORD)", "file:$(FILE_PATH)"],
+       "args": ["start", "--log", "$(LOG_LEVEL)", "--user", "$(SURREAL_USER)", "--pass", "$(SURREAL_PASSWORD)", "file://data/db"],
        "env": [
         {
          name: "FILE_PATH",
