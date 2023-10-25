25f6a6bff84a124aa04f7e9758e911ee38146536
Björn Urban <burban@inovex.de>
2023-10-25 21:23:42 +0200

    fix: update directus

--- l2/providers/Manual/Directus.ts
+++ l2/providers/Manual/Directus.ts
@@ -6,7 +6,7 @@ import {ConfigMap, Namespace, Secret} from "@pulumi/kubernetes/core/v1";
 import {keelAnnotationsProd} from "../../../util/globals";
 
 export function createDirectusManual(namespace: Namespace, secret: Secret, config: ConfigMap) {
-  const website =  new WebService("directus", "cms.tecios.de", namespace, "directus/directus", "10.6.4", {}, "prod");
+  const website =  new WebService("directus", "cms.tecios.de", namespace, "directus/directus", "10.7.0", {}, "prod");
 
   const deployment = createDirectusDeployments(website, secret, config);
   const service = createDirectusService(website);
