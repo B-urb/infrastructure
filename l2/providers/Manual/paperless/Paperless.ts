fe4f9db954df4187fa3c145bc353d534300289f5
Björn Urban <burban@inovex.de>
2023-12-18 22:45:25 +0100

    fix: paperless memory

--- l2/providers/Manual/paperless/Paperless.ts
+++ l2/providers/Manual/paperless/Paperless.ts
@@ -13,10 +13,10 @@ export function createPaperless(namespace: Namespace, secret: Secret, config: Co
       namespace: namespace.metadata.name
     },
     spec: {
-      selector: { matchLabels: { app: "tika" } },
+      selector: {matchLabels: {app: "tika"}},
       replicas: 1,
       template: {
-        metadata: { labels: { app: "tika" } },
+        metadata: {labels: {app: "tika"}},
         spec: {
           containers: [{
             name: "tika",
@@ -34,8 +34,8 @@ export function createPaperless(namespace: Namespace, secret: Secret, config: Co
       namespace: namespace.metadata.name
     },
     spec: {
-      selector: { app: "tika" },
-      ports: [{ port: 80, targetPort: 9998 }],
+      selector: {app: "tika"},
+      ports: [{port: 80, targetPort: 9998}],
       type: "ClusterIP",
     },
   });
@@ -47,10 +47,10 @@ export function createPaperless(namespace: Namespace, secret: Secret, config: Co
       namespace: namespace.metadata.name
     },
     spec: {
-      selector: { matchLabels: { app: "gotenberg" } },
+      selector: {matchLabels: {app: "gotenberg"}},
       replicas: 1,
       template: {
-        metadata: { labels: { app: "gotenberg" } },
+        metadata: {labels: {app: "gotenberg"}},
         spec: {
           containers: [{
             name: "gotenberg",
@@ -73,8 +73,8 @@ export function createPaperless(namespace: Namespace, secret: Secret, config: Co
       namespace: namespace.metadata.name
     },
     spec: {
-      selector: { app: "gotenberg" },
-      ports: [{ port: 80, targetPort: 3000 }],
+      selector: {app: "gotenberg"},
+      ports: [{port: 80, targetPort: 3000}],
       type: "ClusterIP",
     },
   });
@@ -85,146 +85,170 @@ export function createPaperless(namespace: Namespace, secret: Secret, config: Co
       namespace: namespace.metadata.name
     },
     spec: {
-      selector: { app: "paperless" },
-      ports: [{ port: 80, targetPort: 8000 }],
+      selector: {app: "paperless"},
+      ports: [{port: 80, targetPort: 8000}],
       type: "ClusterIP",
     },
   });
 // Define PVCs for Paperless
-const paperlessDataPvc = new k8s.core.v1.PersistentVolumeClaim("paperless-data-pvc", {
-  metadata: {
-    name: "paperless-data",
-    namespace: namespace.metadata.name
-  },
-  spec: {
-    accessModes: ["ReadWriteOnce"],
-    resources: {
-      requests: {
-        storage: "10Gi", // Adjust the size as needed
-      },
+  const paperlessDataPvc = new k8s.core.v1.PersistentVolumeClaim("paperless-data-pvc", {
+    metadata: {
+      name: "paperless-data",
+      namespace: namespace.metadata.name
     },
-    storageClassName: "local-path", // Using the default storage class
-  },
-});
-
-const paperlessMediaPvc = new k8s.core.v1.PersistentVolumeClaim("paperless-media-pvc", {
-  metadata: {
-    name: "paperless-media",
-    namespace: namespace.metadata.name
-  },
-  spec: {
-    accessModes: ["ReadWriteOnce"],
-    resources: {
-      requests: {
-        storage: "10Gi", // Adjust the size as needed
+    spec: {
+      accessModes: ["ReadWriteOnce"],
+      resources: {
+        requests: {
+          storage: "10Gi", // Adjust the size as needed
+        },
       },
+      storageClassName: "local-path", // Using the default storage class
     },
-    storageClassName: "local-path", // Using the default storage class
-  },
-});
+  });
 
+  const paperlessMediaPvc = new k8s.core.v1.PersistentVolumeClaim("paperless-media-pvc", {
+    metadata: {
+      name: "paperless-media",
+      namespace: namespace.metadata.name
+    },
+    spec: {
+      accessModes: ["ReadWriteOnce"],
+      resources: {
+        requests: {
+          storage: "10Gi", // Adjust the size as needed
+        },
+      },
+      storageClassName: "local-path", // Using the default storage class
+    },
+  });
 
 
 // Paperless Deployment
-const paperlessDeployment = new k8s.apps.v1.Deployment("paperless-deployment", {
-  metadata: {
-    name: "paperless",
-    namespace: namespace.metadata.name,
-    labels: {
-      name: "paperless"
-    }
-  },
-  spec: {
-    selector: { matchLabels: { app: "paperless" } },
-    replicas: 1,
-    template: {
-      metadata: { labels: { app: "paperless" } },
-      spec: {
-        containers: [{
-          name: "paperless",
-          image: "ghcr.io/paperless-ngx/paperless-ngx:latest",
-          ports: [{ containerPort: 8000 }],
-          env: [
-            { name: "PAPERLESS_REDIS", value: "redis://redis-redis-master.redis:6379" },
-            { name: "PAPERLESS_URL", value: "https://"+url },
-            { name: "PAPERLESS_PORT", value: "8000" },
-            { name: "PAPERLESS_DBHOST", valueFrom:{secretKeyRef:{name: secret.metadata.name, key: "postgresHost"}}},
-            { name: "PAPERLESS_DBUSER", valueFrom:{secretKeyRef:{name: secret.metadata.name, key: "postgresUser"}}},
-            { name: "PAPERLESS_DBPASS", valueFrom:{secretKeyRef:{name: secret.metadata.name, key: "postgresPassword"}}},
-            { name: "PAPERLESS_DBNAME", valueFrom:{secretKeyRef:{name: secret.metadata.name, key: "postgresDBName"}}},
-            { name: "PAPERLESS_TIKA_ENABLED", value: "1" },
-            { name: "PAPERLESS_TIKA_GOTENBERG_ENDPOINT", value: pulumi.interpolate`http://${gotenbergService.metadata.name}.${gotenbergService.metadata.namespace}` },
-            { name: "PAPERLESS_TIKA_ENDPOINT", value: pulumi.interpolate`http://${tikaService.metadata.name}.${tikaService.metadata.namespace}`},
-            {name: "USERMAP_UID", valueFrom:{secretKeyRef:{name: secret.metadata.name, key: "usermap-uid"}}},
-            {name: "USERMAP_GID", valueFrom:{secretKeyRef:{name: secret.metadata.name, key: "usermap-gid"}}},
-            {name: "PAPERLESS_TIME_ZONE", value: "Europe/Berlin" },
-            {name: "PAPERLESS_OCR_LANGUAGE", value: "deu+eng" },
-            {name: "PAPERLESS_SECRET_KEY", valueFrom:{secretKeyRef:{name: secret.metadata.name, key: "secret"}}},
-            {name: "PAPERLESS_ADMIN_USER", valueFrom:{secretKeyRef:{name: secret.metadata.name, key: "adminUser"}}},
-            {name: "PAPERLESS_ADMIN_PASSWORD", valueFrom:{secretKeyRef:{name: secret.metadata.name, key: "adminPassword"}}},
-          ],
-          volumeMounts: [
+  const paperlessDeployment = new k8s.apps.v1.Deployment("paperless-deployment", {
+    metadata: {
+      name: "paperless",
+      namespace: namespace.metadata.name,
+      labels: {
+        name: "paperless"
+      }
+    },
+    spec: {
+      selector: {matchLabels: {app: "paperless"}},
+      replicas: 1,
+      template: {
+        metadata: {labels: {app: "paperless"}},
+        spec: {
+          containers: [{
+            name: "paperless",
+            image: "ghcr.io/paperless-ngx/paperless-ngx:latest",
+            ports: [{containerPort: 8000}],
+            resources: {
+              requests: {
+                memory: "512Mi",
+                cpu: "500m"
+              },
+              limits: {
+                memory: "1Gi",
+                cpu: "1000m"
+              }
+            },
+            env: [
+              {name: "PAPERLESS_REDIS", value: "redis://redis-redis-master.redis:6379"},
+              {name: "PAPERLESS_URL", value: "https://" + url},
+              {name: "PAPERLESS_PORT", value: "8000"},
+              {name: "PAPERLESS_DBHOST", valueFrom: {secretKeyRef: {name: secret.metadata.name, key: "postgresHost"}}},
+              {name: "PAPERLESS_DBUSER", valueFrom: {secretKeyRef: {name: secret.metadata.name, key: "postgresUser"}}},
+              {
+                name: "PAPERLESS_DBPASS",
+                valueFrom: {secretKeyRef: {name: secret.metadata.name, key: "postgresPassword"}}
+              },
+              {
+                name: "PAPERLESS_DBNAME",
+                valueFrom: {secretKeyRef: {name: secret.metadata.name, key: "postgresDBName"}}
+              },
+              {name: "PAPERLESS_TIKA_ENABLED", value: "1"},
+              {
+                name: "PAPERLESS_TIKA_GOTENBERG_ENDPOINT",
+                value: pulumi.interpolate`http://${gotenbergService.metadata.name}.${gotenbergService.metadata.namespace}`
+              },
+              {
+                name: "PAPERLESS_TIKA_ENDPOINT",
+                value: pulumi.interpolate`http://${tikaService.metadata.name}.${tikaService.metadata.namespace}`
+              },
+              {name: "USERMAP_UID", valueFrom: {secretKeyRef: {name: secret.metadata.name, key: "usermap-uid"}}},
+              {name: "USERMAP_GID", valueFrom: {secretKeyRef: {name: secret.metadata.name, key: "usermap-gid"}}},
+              {name: "PAPERLESS_TIME_ZONE", value: "Europe/Berlin"},
+              {name: "PAPERLESS_OCR_LANGUAGE", value: "deu+eng"},
+              {name: "PAPERLESS_SECRET_KEY", valueFrom: {secretKeyRef: {name: secret.metadata.name, key: "secret"}}},
+              {name: "PAPERLESS_ADMIN_USER", valueFrom: {secretKeyRef: {name: secret.metadata.name, key: "adminUser"}}},
+              {
+                name: "PAPERLESS_ADMIN_PASSWORD",
+                valueFrom: {secretKeyRef: {name: secret.metadata.name, key: "adminPassword"}}
+              },
+            ],
+            volumeMounts: [
+              {
+                name: "data",
+                mountPath: "/usr/src/paperless/data",
+              },
+              {
+                name: "media",
+                mountPath: "/usr/src/paperless/media",
+              },
+              // Mount other volumes if necessary
+            ],
+          }],
+          volumes: [
             {
               name: "data",
-              mountPath: "/usr/src/paperless/data",
+              persistentVolumeClaim: {
+                claimName: paperlessDataPvc.metadata.name,
+              },
             },
             {
               name: "media",
-              mountPath: "/usr/src/paperless/media",
+              persistentVolumeClaim: {
+                claimName: paperlessMediaPvc.metadata.name,
+              },
             },
-            // Mount other volumes if necessary
+            // Define other volumes if necessary
           ],
-        }],
-        volumes: [
-          {
-            name: "data",
-            persistentVolumeClaim: {
-              claimName: paperlessDataPvc.metadata.name,
-            },
-          },
-          {
-            name: "media",
-            persistentVolumeClaim: {
-              claimName: paperlessMediaPvc.metadata.name,
-            },
-          },
-          // Define other volumes if necessary
-        ],
+        },
       },
     },
-  },
-});
+  });
 
 
 // Paperless Ingress
-const ingress = new k8s.networking.v1.Ingress("paperless-ingress", {
-      metadata: {
-        name: "paperless",
-        annotations: {
-          "kubernetes.io/ingress.class": "traefik",
-          "cert-manager.io/cluster-issuer": "letsencrypt",
+  const ingress = new k8s.networking.v1.Ingress("paperless-ingress", {
+        metadata: {
+          name: "paperless",
+          annotations: {
+            "kubernetes.io/ingress.class": "traefik",
+            "cert-manager.io/cluster-issuer": "letsencrypt",
+          },
+          namespace: namespace.metadata.name
         },
-        namespace: namespace.metadata.name
-      },
 
-      spec: {
-        tls: [{
-          secretName: "paperless-tls",
-          hosts: [url]
-        }],
-        rules: [
-          {
-            host: url,
-            http: {
-              paths: [{
-                pathType: "Prefix",
-                path: "/",
-                backend: {service: {name: paperlessService.metadata.name, port:{number: 80 }}}
-              }]
-            }
-          }]
+        spec: {
+          tls: [{
+            secretName: "paperless-tls",
+            hosts: [url]
+          }],
+          rules: [
+            {
+              host: url,
+              http: {
+                paths: [{
+                  pathType: "Prefix",
+                  path: "/",
+                  backend: {service: {name: paperlessService.metadata.name, port: {number: 80}}}
+                }]
+              }
+            }]
 
+        }
       }
-    }
-);
+  );
 }
