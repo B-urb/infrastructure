9ae2ed1602f3f42451aef3aa1b0c3a81928da624
Björn Urban <burban@inovex.de>
2024-02-13 22:54:12 +0100

    fix: resource delete

--- flux-operator/index.ts
+++ flux-operator/index.ts
@@ -14,239 +14,262 @@ const operatorVersion = config.get("operator-version") || defaultOperatorVersion
 // Get the Pulumi API token.
 const pulumiAccessToken = config.requireSecret("pulumiAccessToken")
 
-// Create the API token as a Kubernetes Secret.
-const accessToken = new Secret("accesstoken", {
-    stringData: { accessToken: pulumiAccessToken},
-});
 const stackCRD = new kubernetes.yaml.ConfigFile("stackcrd", {
-    file: `https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/${crdVersion}/deploy/crds/pulumi.com_stacks.yaml`
+  file: `https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/${crdVersion}/deploy/crds/pulumi.com_stacks.yaml`
 });
 const programCRD = new kubernetes.yaml.ConfigFile("programcrd", {
-    file: `https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/${crdVersion}/deploy/crds/pulumi.com_programs.yaml`
+  file: `https://raw.githubusercontent.com/pulumi/pulumi-kubernetes-operator/${crdVersion}/deploy/crds/pulumi.com_programs.yaml`
 });
 
-const deploymentOptions = { dependsOn: [stackCRD, programCRD] };
+const deploymentOptions = {dependsOn: [stackCRD, programCRD]};
 
 for (let ns of deployNamespaceList) {
-    const operatorServiceAccount = new kubernetes.core.v1.ServiceAccount(`operator-service-account-${ns}`, {
-        metadata: {
-            "namespace": ns,
-        },
-    });
+  const operatorServiceAccount = new kubernetes.core.v1.ServiceAccount(`operator-service-account-${ns}`, {
+    metadata: {
+      "namespace": ns,
+    },
+  });
 
-    const operatorRole = new kubernetes.rbac.v1.Role(`operator-role-${ns}`, {
-        metadata: {
-            "namespace": ns,
-        },
-        rules: [
-            {
-                apiGroups: [""],
-                resources: [
-                    "pods",
-                    "services",
-                    "services/finalizers",
-                    "endpoints",
-                    "persistentvolumeclaims",
-                    "events",
-                    "configmaps",
-                    "secrets",
-                ],
-                verbs: [
-                    "create",
-                    "delete",
-                    "get",
-                    "list",
-                    "patch",
-                    "update",
-                    "watch",
-                ],
-            },
-            {
-                apiGroups: ["apps"],
-                resources: [
-                    "deployments",
-                    "daemonsets",
-                    "replicasets",
-                    "statefulsets",
-                ],
-                verbs: [
-                    "create",
-                    "delete",
-                    "get",
-                    "list",
-                    "patch",
-                    "update",
-                    "watch",
-                ],
-            },
-            {
-                apiGroups: ["monitoring.coreos.com"],
-                resources: ["servicemonitors"],
-                verbs: [
-                    "create",
-                    "get",
-                ],
-            },
-            {
-                apiGroups: ["apps"],
-                resourceNames: ["pulumi-kubernetes-operator"],
-                resources: ["deployments/finalizers"],
-                verbs: ["update"],
-            },
-            {
-                apiGroups: [""],
-                resources: ["pods"],
-                verbs: ["get"],
-            },
-            {
-                apiGroups: ["apps"],
-                resources: [
-                    "replicasets",
-                    "deployments",
-                ],
-                verbs: ["get"],
-            },
-            {
-                apiGroups: ["pulumi.com"],
-                resources: ["*"],
-                verbs: [
-                    "create",
-                    "delete",
-                    "get",
-                    "list",
-                    "patch",
-                    "update",
-                    "watch",
-                ],
-            },
-            {
-                apiGroups: ["coordination.k8s.io"],
-                resources: ["leases"],
-                verbs: [
-                    "create",
-                    "get",
-                    "list",
-                    "update",
-                ],
-            },
+  const operatorRole = new kubernetes.rbac.v1.Role(`operator-role-${ns}`, {
+    metadata: {
+      "namespace": ns,
+    },
+    rules: [
+      {
+        apiGroups: [""],
+        resources: [
+          "pods",
+          "services",
+          "services/finalizers",
+          "endpoints",
+          "persistentvolumeclaims",
+          "events",
+          "configmaps",
+          "secrets",
+          "namespaces"
         ],
-    });
+        verbs: [
+          "create",
+          "delete",
+          "get",
+          "list",
+          "patch",
+          "update",
+          "watch",
+        ],
+      },
+      {
+        apiGroups: ["apps"],
+        resources: [
+          "deployments",
+          "daemonsets",
+          "replicasets",
+          "statefulsets",
+        ],
+        verbs: [
+          "create",
+          "delete",
+          "get",
+          "list",
+          "patch",
+          "update",
+          "watch",
+        ],
+      },
+      {
+        apiGroups: ["monitoring.coreos.com"],
+        resources: ["servicemonitors"],
+        verbs: [
+          "create",
+          "get",
+        ],
+      },
+      {
+        apiGroups: ["apps"],
+        resourceNames: ["pulumi-kubernetes-operator"],
+        resources: ["deployments/finalizers"],
+        verbs: ["update"],
+      },
+      {
+        apiGroups: [""],
+        resources: ["pods"],
+        verbs: ["get"],
+      },
+      {
+        apiGroups: ["apps"],
+        resources: [
+          "replicasets",
+          "deployments",
+        ],
+        verbs: ["get"],
+      },
+      {
+        apiGroups: ["pulumi.com"],
+        resources: ["*"],
+        verbs: [
+          "create",
+          "delete",
+          "get",
+          "list",
+          "patch",
+          "update",
+          "watch",
+        ],
+      },
+      {
+        apiGroups: ["coordination.k8s.io"],
+        resources: ["leases"],
+        verbs: [
+          "create",
+          "get",
+          "list",
+          "update",
+        ],
+      },
+    ],
+  });
 
-    const operatorRoleBinding = new kubernetes.rbac.v1.RoleBinding(`operator-role-binding-${ns}`, {
-        metadata: {
-            "namespace": ns,
-        },
-        subjects: [{
-            kind: "ServiceAccount",
-            name: operatorServiceAccount.metadata.name,
-        }],
-        roleRef: {
-            kind: "Role",
-            name: operatorRole.metadata.name,
-            apiGroup: "rbac.authorization.k8s.io",
-        },
-    });
+  const operatorRoleBinding = new kubernetes.rbac.v1.RoleBinding(`operator-role-binding-${ns}`, {
+    metadata: {
+      "namespace": ns,
+    },
+    subjects: [{
+      kind: "ServiceAccount",
+      name: operatorServiceAccount.metadata.name,
+    }],
+    roleRef: {
+      kind: "Role",
+      name: operatorRole.metadata.name,
+      apiGroup: "rbac.authorization.k8s.io",
+    },
+  });
 
-    const operatorDeployment = new kubernetes.apps.v1.Deployment(`pulumi-kubernetes-operator-${ns}`, {
+  const operatorDeployment = new kubernetes.apps.v1.Deployment(`pulumi-kubernetes-operator-${ns}`, {
+    metadata: {
+      "namespace": ns,
+    },
+    spec: {
+      replicas: 1,
+      selector: {
+        matchLabels: {
+          name: "pulumi-kubernetes-operator",
+        },
+      },
+      template: {
         metadata: {
-            "namespace": ns,
+          labels: {
+            name: "pulumi-kubernetes-operator",
+          },
         },
         spec: {
-            replicas: 1,
-            selector: {
-                matchLabels: {
-                    name: "pulumi-kubernetes-operator",
+          serviceAccountName: operatorServiceAccount.metadata.name,
+          containers: [{
+            name: "pulumi-kubernetes-operator",
+            image: image,
+            args: ["--zap-level=error", "--zap-time-encoding=iso8601"],
+            imagePullPolicy: "Always",
+            env: [
+              {
+                name: "WATCH_NAMESPACE",
+                valueFrom: {
+                  fieldRef: {
+                    fieldPath: "metadata.namespace",
+                  },
                 },
-            },
-            template: {
-                metadata: {
-                    labels: {
-                        name: "pulumi-kubernetes-operator",
-                    },
+              },
+              {
+                name: "POD_NAME",
+                valueFrom: {
+                  fieldRef: {
+                    fieldPath: "metadata.name",
+                  },
                 },
-                spec: {
-                    serviceAccountName: operatorServiceAccount.metadata.name,
-                    containers: [{
-                        name: "pulumi-kubernetes-operator",
-                        image: image,
-                        args: ["--zap-level=error", "--zap-time-encoding=iso8601"],
-                        imagePullPolicy: "Always",
-                        env: [
-                            {
-                                name: "WATCH_NAMESPACE",
-                                valueFrom: {
-                                    fieldRef: {
-                                        fieldPath: "metadata.namespace",
-                                    },
-                                },
-                            },
-                            {
-                                name: "POD_NAME",
-                                valueFrom: {
-                                    fieldRef: {
-                                        fieldPath: "metadata.name",
-                                    },
-                                },
-                            },
-                            {
-                                name: "OPERATOR_NAME",
-                                value: "pulumi-kubernetes-operator",
-                            },
-                            {
-                                name: "GRACEFUL_SHUTDOWN_TIMEOUT_DURATION",
-                                value: "5m",
-                            },
-                            {
-                                name: "MAX_CONCURRENT_RECONCILES",
-                                value: "10",
-                            },
+              },
+              {
+                name: "OPERATOR_NAME",
+                value: "pulumi-kubernetes-operator",
+              },
+              {
+                name: "GRACEFUL_SHUTDOWN_TIMEOUT_DURATION",
+                value: "5m",
+              },
+              {
+                name: "MAX_CONCURRENT_RECONCILES",
+                value: "10",
+              },
 
 
-                        ],
-                    }],
-                    // Should be same or larger than GRACEFUL_SHUTDOWN_TIMEOUT_DURATION
-                    terminationGracePeriodSeconds: 300,
-                },
-            },
-        },
-    }, deploymentOptions);
-
-// Create an NGINX deployment in-cluster.
-    const stackL1 = new CustomResource("l1", {
-        apiVersion: 'pulumi.com/v1',
-        kind: 'Stack',
-        metadata: {
-            name: "l1",
-            namespace: ns
-        },
-        spec: {
-            accessTokenSecret: accessToken.metadata.name,
-            stack: "hetzner",
-            projectRepo: "https://github.com/tecios/infrastructure",
-            repoDir: "/l1",
-            branch: "development",
-            retryOnUpdateConflict: true,
-            destroyOnFinalize: true,
-        }
-    });
-    const stackL2 = new CustomResource("l2", {
-        apiVersion: 'pulumi.com/v1',
-        kind: 'Stack',
-        metadata: {
-            name: "l2",
-            namespace: ns
-        },
-        spec: {
-            accessTokenSecret: accessToken.metadata.name,
-            stack: "hetzner",
-            projectRepo: "https://github.com/tecios/infrastructure",
-            repoDir: "/l2",
-            branch: "development",
-            prerequisites: [
-                {name: "l1"}
             ],
-            retryOnUpdateConflict: true,
-            destroyOnFinalize: true,
-        }
+          }],
+          // Should be same or larger than GRACEFUL_SHUTDOWN_TIMEOUT_DURATION
+          terminationGracePeriodSeconds: 300,
+        },
+      },
+    },
+  }, deploymentOptions);
+
+// Create the API token as a Kubernetes Secret.
+  const accessToken = new Secret("accesstoken", {
+    metadata: {
+      name: "flux-secret",
+      namespace: ns
+    },
+    stringData: {accessToken: pulumiAccessToken},
+  });
+// Create an NGINX deployment in-cluster.
+  const stackL1 = new CustomResource("l1", {
+    apiVersion: 'pulumi.com/v1',
+    kind: 'Stack',
+    metadata: {
+      name: "l1",
+      namespace: ns
+    },
+      spec: {
+        envRefs: {
+          PULUMI_ACCESS_TOKEN: {
+            type: "Secret",
+            secret: {
+              key: "accessToken",
+              name: accessToken.metadata.name,
+              namespace: ns,
+            },
+          }
+        },
+        stack: "hetzner",
+        projectRepo: "https://github.com/tecios/infrastructure",
+        repoDir: "/l1",
+        branch: "development",
+        retryOnUpdateConflict: true,
+        destroyOnFinalize: true,
+      }
     });
+  const stackL2 = new CustomResource("l2", {
+    apiVersion: 'pulumi.com/v1',
+    kind: 'Stack',
+    metadata: {
+      name: "l2",
+      namespace: ns
+    },
+    spec: {
+      envRefs: {
+        PULUMI_ACCESS_TOKEN: {
+          type: "Secret",
+          secret: {
+            key: "accessToken",
+            name: accessToken.metadata.name,
+            namespace: ns,
+          },
+        }
+      },
+      stack: "hetzner",
+      projectRepo: "https://github.com/tecios/infrastructure",
+      repoDir: "/l2",
+      branch: "development",
+      prerequisites: [
+        {name: "l1"}
+      ],
+      retryOnUpdateConflict: true,
+      destroyOnFinalize: true,
+    }
+  });
 }
