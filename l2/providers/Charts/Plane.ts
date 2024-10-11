e50342758b7bbf2075c62f312bdaa567b41b8639
Björn Urban <burban@inovex.de>
2024-10-11 17:52:43 +0200

    fix: type

--- l2/providers/Charts/Plane.ts
+++ l2/providers/Charts/Plane.ts
@@ -6,7 +6,7 @@ import {Namespace} from "@pulumi/kubernetes/core/v1";
 export default function createPlaneHelm(namespace: Namespace, config: {
   awsS3Bucket: Output<string>;
   s3UserSecret: Output<string>;
-  awsS3Region: Output<"af-south-1" | "ap-east-1" | "ap-northeast-1" | "ap-northeast-2" | "ap-northeast-3" | "ap-south-1" | "ap-south-2" | "ap-southeast-1" | "ap-southeast-2" | "ap-southeast-3" | "ap-southeast-4" | "ca-central-1" | "ca-west-1" | "cn-north-1" | "cn-northwest-1" | "eu-central-1" | "eu-central-2" | "eu-north-1" | "eu-south-1" | "eu-south-2" | "eu-west-1" | "eu-west-2" | "eu-west-3" | "me-central-1" | "me-south-1" | "sa-east-1" | "us-gov-east-1" | "us-gov-west-1" | "us-east-1" | "us-east-2" | "us-west-1" | "us-west-2"> & Lifted<"af-south-1" | "ap-east-1" | "ap-northeast-1" | "ap-northeast-2" | "ap-northeast-3" | "ap-south-1" | "ap-south-2" | "ap-southeast-1" | "ap-southeast-2" | "ap-southeast-3" | "ap-southeast-4" | "ca-central-1" | "ca-west-1" | "cn-north-1" | "cn-northwest-1" | "eu-central-1" | "eu-central-2" | "eu-north-1" | "eu-south-1" | "eu-south-2" | "eu-west-1" | "eu-west-2" | "eu-west-3" | "me-central-1" | "me-south-1" | "sa-east-1" | "us-gov-east-1" | "us-gov-west-1" | "us-east-1" | "us-east-2" | "us-west-1" | "us-west-2">;
+  awsS3Region: Output<string>;
   planeKey: Output<string>;
   dbName: Output<string>;
   redisConnectionString: string;
